3ce3d6e711239ece91255c5e6dfe5a17882107d4
Md Ashiqul Islam <ashiqul.cel@gmail.com>
2021-12-15 18:16:10 +0600

    update

--- 6 Stored Procedures/Unilever/Get_Damage_Report_Trade_Return.sql
+++ 6 Stored Procedures/Unilever/Get_Damage_Report_Trade_Return.sql
@@ -1,10 +1,12 @@
 USE [UnileverOS]
 GO
 
-ALTER PROCEDURE [dbo].[Get_Damage_Report_Trade_Return]
-@SalesPointIDs VARCHAR(5000), @StartDate DATETIME, @EndDate DATETIME
-AS
-SET NOCOUNT ON;
+--ALTER PROCEDURE [dbo].[Get_Damage_Report_Trade_Return]
+--@SalesPointIDs VARCHAR(5000), @StartDate DATETIME, @EndDate DATETIME
+--AS
+--SET NOCOUNT ON;
+
+DECLARE @SalesPointIDs VARCHAR(5000) = 62, @StartDate DATETIME = '1 Nov 2021', @EndDate DATETIME = '30 Nov 2021'
 
 SELECT 'Region', 'Area', 'Territory', 'Town', 'OutletCode', 'OutletName',
 'Channel', 'Route', 'SSO', 'SkuCode', 'SKUDescription', 'PackSize',
@@ -15,9 +17,9 @@ SELECT 'Region', 'Area', 'Territory', 'Town', 'OutletCode', 'OutletName',
 UNION ALL
 
 Select M3.Name Region, M2.Name Area, DOWSD.TerritoryName, SP.TownName, DOWSD.OutletCode, DOWSD.OutletName,
-DOWSD.ChannelName, DOWSD.RouteName, DOWSD.SRName, DOWSD.SKUCode, DOWSD.SKUName, CAST(vs.CartonPcsRatio AS VARCHAR), 
-DOWSD.ChildReasonDescription, CAST(FLOOR(DOWSD.DamageQty/ NULLIF(vs.CartonPcsRatio,0)) AS VARCHAR), CAST((DOWSD.DamageQty % vs.CartonPcsRatio) AS VARCHAR), 
-CAST((DOWSD.TradePrice * vs.CartonPcsRatio) AS VARCHAR), CAST((DOWSD.DamageQty * DOWSD.ClaimPrice) AS VARCHAR), CAST((DOWSD.SecondarySalesQty*DOWSD.ClaimPrice) AS VARCHAR),
+DOWSD.ChannelName, DOWSD.RouteName, DOWSD.SRName, DOWSD.SKUCode, DOWSD.SKUName, CAST(dowsd.ConversionValue AS VARCHAR), 
+DOWSD.ChildReasonDescription, CAST(FLOOR(DOWSD.DamageQty/ NULLIF(dowsd.ConversionValue, 0)) AS VARCHAR), CAST((DOWSD.DamageQty % dowsd.ConversionValue) AS VARCHAR), 
+CAST((DOWSD.TradePrice * dowsd.ConversionValue) AS VARCHAR), CAST((DOWSD.DamageQty * DOWSD.ClaimPrice) AS VARCHAR), CAST((DOWSD.SecondarySalesQty*DOWSD.ClaimPrice) AS VARCHAR),
 CAST((((DOWSD.DamageQty * DOWSD.ClaimPrice) / NULLIF(DOWSD.SecondarySalesQty*DOWSD.ClaimPrice, 0)) * 100) AS VARCHAR), DOWSD.CompanyCode
 
 from  ReportDailyOutletWiseSKUDamage DOWSD 
@@ -25,6 +27,5 @@ INNER JOIN SalesPoints SP ON DOWSD.DistributorID=SP.SalesPointID
 INNER JOIN MHNode M ON M.NodeID = DOWSD.TerritoryID
 INNER JOIN MHNode M2 ON M2.NodeID = M.ParentID
 INNER JOIN MHNode M3 ON M3.NodeID = M2.ParentID
-INNER JOIN View_SKUs AS vs ON DOWSD.SKUID = vs.SKUID
 Where DOWSD.DistributorID IN (SELECT NUMBER FROM dbo.STRING_TO_INT(@SalesPointIDs))
 AND CAST(DOWSD.[Date] AS DATE) BETWEEN CAST(@StartDate AS DATE) AND CAST(@EndDate AS DATE)
--- 6 Stored Procedures/Unilever/rptSKUCapping.sql
+++ 6 Stored Procedures/Unilever/rptSKUCapping.sql
@@ -1,10 +1,44 @@
 USE [UnileverOS]
 GO
 
-CREATE PROCEDURE [dbo].[rptSKUCapping]
-@SalesControlSetupID INT
+ALTER PROCEDURE [dbo].[rptSKUCapping]
+@SalesControlSetupID INT, @SalesPointID INT, @StartDate DATETIME, @EndDate DATETIME
 AS
 SET NOCOUNT ON;
-SELECT RegionName, AreaName, TerritoryName, TownCode, TownName, RouteName, OutletCode, OutletName, MaxCeil, PendingOrderIssueQty, IssueQty, SalesQty
-FROM ReportSKUCappingSummary
-WHERE SalesControlSetupID = @SalesControlSetupID
+
+--DECLARE @SalesControlSetupID INT = 2004, @SalesPointID INT = 22, @StartDate DATETIME = '1 Nov 2021', @EndDate DATETIME = '14 Dec 2021'
+
+SELECT MHR.Name Region, MHA.Name Area, MHT.Name Territory, sp.TownName,
+r.Name RouteName, c.Code OutletCode, c.Name OutletName, c2.Name ChannelName,
+ISNULL(sci.MaxCeil, scs.MaxCeil) MaxCeil, ISNULL(X.PendingOrderIssueQty, 0) PendingOrderIssueQty, ISNULL(rds.IssuedQty, 0) IssuedQty, ISNULL(rds.ConfirmedDeliveryQty, 0) SalesQty
+FROM SalesPoints AS sp
+INNER JOIN Customers AS c ON sp.SalesPointID = c.SalesPointID
+INNER JOIN Routes AS r ON c.RouteID = r.RouteID
+INNER JOIN Channels AS c2 ON c.ChannelID = c2.ChannelID
+INNER JOIN SalesPointMHNodes SPMH ON SPMH.SalesPointID = sp.SalesPointID
+INNER JOIN MHNode MHT ON SPMH.NodeID = MHT.NodeID
+INNER JOIN MHNode MHA ON MHT.ParentID = MHA.NodeID
+INNER JOIN MHNode MHR ON MHA.ParentID = MHR.NodeID
+INNER JOIN SalesControlSetup AS scs ON scs.SalesControlSetupID = @SalesControlSetupID
+LEFT JOIN
+(
+	SELECT RDS.salespointid, RDS.OutletCode, rds.SKUID, SUM(rds.IssuedQty) IssuedQty, SUM(rds.ConfirmedDeliveryQty) ConfirmedDeliveryQty
+	FROM ReportDailyOutletSKUOrderVsDelivery rds
+	WHERE CAST(rds.SalesDate AS DATE) BETWEEN CAST(@StartDate AS DATE) AND CAST(@EndDate AS DATE)
+	GROUP BY RDS.salespointid, RDS.OutletCode, rds.SKUID
+) RDS ON sp.salespointid = RDS.salespointid AND c.Code = RDS.OutletCode AND RDS.SKUID = scs.SKUID
+LEFT JOIN
+(
+	SELECT so.salespointid, so.CustomerID, soi.SKUID, SUM(soi.Quantity) PendingOrderIssueQty
+	FROM SalesOrders so
+	INNER JOIN SalesOrderItem AS soi ON so.OrderID = soi.OrderID
+	WHERE CAST(so.OrderDate AS DATE) BETWEEN CAST(@StartDate AS DATE) AND CAST(@EndDate AS DATE)
+	GROUP BY so.salespointid, so.CustomerID, soi.SKUID
+)X ON sp.salespointid = X.salespointid AND c.CustomerID = X.CustomerID AND scs.SKUID = X.SKUID
+LEFT JOIN
+(
+	SELECT sci.SalesPointID, sci.OutletCode, sci.MaxCeil
+	FROM SKUSalesControlItems AS sci WHERE sci.SalesControlSetupID = @SalesControlSetupID
+) sci ON sp.SalesPointID = sci.SalesPointID AND c.Code = sci.OutletCode
+WHERE sp.SalesPointID = @SalesPointID AND c.[Status] = 16
+ORDER BY c.Code
