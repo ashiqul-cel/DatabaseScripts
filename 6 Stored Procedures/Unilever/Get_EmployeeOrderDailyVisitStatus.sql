2988f6d824de9d1c07cac0842095614a8cf689e1
Md Ashiqul Islam <ashiqul.cel@gmail.com>
2021-12-20 18:23:11 +0600

    update

--- 6 Stored Procedures/Unilever/Get_EmployeeOrderDailyVisitStatus.sql
+++ 6 Stored Procedures/Unilever/Get_EmployeeOrderDailyVisitStatus.sql
@@ -6,61 +6,63 @@ CREATE PROCEDURE [dbo].[Get_EmployeeOrderDailyVisitStatus]
 AS
 SET NOCOUNT ON;
 
+--DECLARE @OrderDate DATETIME = '20 Dec 2021', @SalesPointID INT = 10, @EmployeeID INT = 15952, @RouteID INT = 13089, @SectionID INT = 59042
+
 DECLARE @RptDataChecker INT;
 SET @RptDataChecker = ISNULL((SELECT COUNT(*) FROM SalesOrdersRpt 
                       WHERE CAST(OrderDate AS DATE) = CAST(@OrderDate AS DATE)), 0);
 
 IF(ISNULL(@RptDataChecker, 0) > 0)
-BEGIN
+	BEGIN
 
-SELECT so.OrderID, so.OrderDate, so.CustomerID, so.SRID, 
-c.Code CustomerCode, c.Name OutletName, c.Address1 + ' ' + c.Address2 AS [Address], 
-c.ContactNo, r.Name [ROUTE], MIN(so.CheckInTime) StartTime, MAX(so.CheckOutTime) EndTime, 
-ISNULL((Sum (DATEDIFF(SECOND, so.CheckInTime, so.CheckOutTime))), 0) AS TotalTime, so.GrossValue,
+		SELECT so.OrderID, so.OrderDate, so.CustomerID, so.SRID, 
+		c.Code CustomerCode, c.Name OutletName, c.Address1 + ' ' + c.Address2 AS [Address], 
+		c.ContactNo, r.Name [ROUTE], MIN(so.CheckInTime) StartTime, MAX(so.CheckOutTime) EndTime, 
+		ISNULL((Sum (DATEDIFF(SECOND, so.CheckInTime, so.CheckOutTime))), 0) AS TotalTime, so.GrossValue,
 
-LPC = (SELECT count(b.SKUID) FROM SalesOrderRpt as a 
-INNER JOIN SalesOrderItemRpt as b on a.OrderID=b.OrderID
-WHERE a.SalesPointID = @SalesPointID AND a.SRID= @EmployeeID 
-AND a.OrderDate=@OrderDate AND a.SectionID=@SectionID AND a.CustomerID=so.CustomerID),
+		LPC = (SELECT count(b.SKUID) FROM SalesOrdersRpt as a
+		INNER JOIN SalesOrderItemRpt as b on a.OrderID=b.OrderID
+		WHERE a.SalesPointID = @SalesPointID AND a.SRID= @EmployeeID 
+		AND a.OrderDate=@OrderDate AND a.SectionID=@SectionID AND a.CustomerID=so.CustomerID),
 
-NoOrderReasonName = (SELECT nor.Name FROM NoOrderReasons AS nor 
-WHERE nor.NoOrderReasonID=so.NoOrderReasonID)
+		NoOrderReasonName = (SELECT nor.Name FROM NoOrderReasons AS nor 
+		WHERE nor.NoOrderReasonID=so.NoOrderReasonID)
 
-FROM [SalesOrderRpt] AS so 
-INNER JOIN [Customers] AS c ON c.CustomerID = so.CustomerID
-INNER JOIN [Routes] r ON r.RouteID = c.RouteID     
+		FROM SalesOrdersRpt AS so
+		INNER JOIN [Customers] AS c ON c.CustomerID = so.CustomerID
+		INNER JOIN [Routes] r ON r.RouteID = c.RouteID     
 
-WHERE so.SalesPointID = @SalesPointID AND so.OrderDate = @OrderDate 
-AND so.SRID = @EmployeeID AND so.RouteID = @RouteID AND so.SectionID = @SectionID
+		WHERE so.SalesPointID = @SalesPointID AND so.OrderDate = @OrderDate 
+		AND so.SRID = @EmployeeID AND so.RouteID = @RouteID AND so.SectionID = @SectionID
 
-GROUP BY so.CustomerID, so.SRID, c.Code, c.Name, c.Address1 + ' ' + c.Address2, 
-c.ContactNo, r.Name, so.OrderDate, so.GrossValue, so.OrderID, so.NoOrderReasonID;
+		GROUP BY so.CustomerID, so.SRID, c.Code, c.Name, c.Address1 + ' ' + c.Address2, 
+		c.ContactNo, r.Name, so.OrderDate, so.GrossValue, so.OrderID, so.NoOrderReasonID;
 
-END
+	END
 ELSE
-BEGIN
+	BEGIN
 
-SELECT so.OrderID, so.OrderDate, so.CustomerID, so.SRID, 
-c.Code CustomerCode, c.Name OutletName, c.Address1 + ' ' + c.Address2 AS [Address], 
-c.ContactNo, r.Name [ROUTE], MIN(so.CheckInTime) StartTime, MAX(so.CheckOutTime) EndTime, 
-ISNULL((Sum (DATEDIFF(SECOND, so.CheckInTime, so.CheckOutTime))), 0) AS TotalTime, so.GrossValue,
+		SELECT so.OrderID, so.OrderDate, so.CustomerID, so.SRID, 
+		c.Code CustomerCode, c.Name OutletName, c.Address1 + ' ' + c.Address2 AS [Address], 
+		c.ContactNo, r.Name [ROUTE], MIN(so.CheckInTime) StartTime, MAX(so.CheckOutTime) EndTime, 
+		ISNULL((Sum (DATEDIFF(SECOND, so.CheckInTime, so.CheckOutTime))), 0) AS TotalTime, so.GrossValue,
 
-LPC = (SELECT count(b.SKUID) FROM SalesOrders as a 
-INNER JOIN SalesOrderItem as b on a.OrderID=b.OrderID
-WHERE a.SalesPointID = @SalesPointID AND a.SRID= @EmployeeID 
-AND a.OrderDate=@OrderDate AND a.SectionID=@SectionID AND a.CustomerID=so.CustomerID),
+		LPC = (SELECT count(b.SKUID) FROM SalesOrders as a 
+		INNER JOIN SalesOrderItem as b on a.OrderID=b.OrderID
+		WHERE a.SalesPointID = @SalesPointID AND a.SRID= @EmployeeID 
+		AND a.OrderDate=@OrderDate AND a.SectionID=@SectionID AND a.CustomerID=so.CustomerID),
 
-NoOrderReasonName = (SELECT nor.Name FROM NoOrderReasons AS nor 
-WHERE nor.NoOrderReasonID=so.NoOrderReasonID)
+		NoOrderReasonName = (SELECT nor.Name FROM NoOrderReasons AS nor 
+		WHERE nor.NoOrderReasonID=so.NoOrderReasonID)
 
-FROM [SalesOrders] AS so 
-INNER JOIN [Customers] AS c ON c.CustomerID = so.CustomerID
-INNER JOIN [Routes] r ON r.RouteID = c.RouteID     
+		FROM [SalesOrders] AS so 
+		INNER JOIN [Customers] AS c ON c.CustomerID = so.CustomerID
+		INNER JOIN [Routes] r ON r.RouteID = c.RouteID     
 
-WHERE so.SalesPointID = @SalesPointID AND so.OrderDate = @OrderDate 
-AND so.SRID = @EmployeeID AND so.RouteID = @RouteID AND so.SectionID = @SectionID
+		WHERE so.SalesPointID = @SalesPointID AND so.OrderDate = @OrderDate 
+		AND so.SRID = @EmployeeID AND so.RouteID = @RouteID AND so.SectionID = @SectionID
 
-GROUP BY so.CustomerID, so.SRID, c.Code, c.Name, c.Address1 + ' ' + c.Address2, 
-c.ContactNo, r.Name, so.OrderDate, so.GrossValue, so.OrderID, so.NoOrderReasonID;
+		GROUP BY so.CustomerID, so.SRID, c.Code, c.Name, c.Address1 + ' ' + c.Address2, 
+		c.ContactNo, r.Name, so.OrderDate, so.GrossValue, so.OrderID, so.NoOrderReasonID;
 
-END
+	END
