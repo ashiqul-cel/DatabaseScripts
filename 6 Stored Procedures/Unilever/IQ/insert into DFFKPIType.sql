4a1a7c8ecf565b641091034b1fb27d65ad655864
Md Ashiqul Islam <ashiqul.cel@gmail.com>
2022-01-05 14:35:18 +0600

    DFF KPI_Type Data insert

---
+++ 6 Stored Procedures/Unilever/IQ/insert into DFFKPIType.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO DFFKPIType
+(
+	DFFKPITypeID,
+	Code,
+	Name,
+	PositionValue,
+	CreatedBy,
+	CreateDate,
+	IsActive
+)
+VALUES
+(29, '129', 'IQ Total Assortment', 29, -9, GETDATE(), 16),
+(30, '130', 'IQ E2E (Easy to Earn)', 30, -9, GETDATE(), 16),
+(31, '131', 'IQ E2S (Easy to Sell)', 31, -9, GETDATE(), 16),
+(32, '132', 'IQ Green Store', 32, -9, GETDATE(), 16);
