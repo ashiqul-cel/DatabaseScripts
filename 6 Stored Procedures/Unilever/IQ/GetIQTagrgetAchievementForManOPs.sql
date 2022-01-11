e32e94ded55f010686b2e1526169d6732b0a0154
ashiqul <ashiqul33@gmail.com>
2022-01-11 21:44:14 +0600

    update based on View_TargetAchievementIQ

--- 6 Stored Procedures/Unilever/IQ/GetIQTagrgetAchievementForManOPs.sql
+++ 6 Stored Procedures/Unilever/IQ/GetIQTagrgetAchievementForManOPs.sql
@@ -1,27 +1,23 @@
-CREATE PROCEDURE [dbo].[GetIQTagrgetAchievementForManOPs]
+ALTER PROCEDURE [dbo].[GetIQTagrgetAchievementForManOPs]
 @SalesPointID INT, @JCYearID INT, @JCMonthID INT
 AS
 SET NOCOUNT ON;
 
-declare @SalesPointID INT = 22, @JCYearID INT = 9, @JCMonthID INT = 107
+--declare @SalesPointID INT = 22, @JCYearID INT = 9, @JCMonthID INT = 107
 
 SELECT
-sum(iqr.EBTarget) EBTarget, sum(iqr.EBAchievement) EBAchievement, iqr.EBThreshold,
-sum(iqr.RLTarget) RLTarget,sum(iqr.RLAchievement) RLAchievement ,iqr.RLThreshold,
-sum(iqr.NPDTarget) NPDTarget,sum(iqr.NPDAchievement) NPDAchievement,iqr.NPDThreshold,
-sum(iqr.WPTarget) WPTarget, sum(iqr.WPAchievement) WPAchievement ,iqr.WPThreshold,
-ik.RedLine, ik.EverBilled, ik.WithPack, ik.NewProduct,
-ISNULL(iqrc.AchievementUBL,0) AchievementUBL, ISNULL(iqrc.AchievementUCL,0)AchievementUCL
+SUM(taiq.EBTarget) EBTarget, SUM(taiq.EBAchievement) EBAchievement, taiq.EBThreshold,
+SUM(taiq.RLTarget) RLTarget, SUM(taiq.RLAchievement) RLAchievement, taiq.RLThreshold,
+SUM(taiq.NPDTarget) NPDTarget, SUM(taiq.NPDAchievement) NPDAchievement, taiq.NPDThreshold,
+SUM(taiq.WPTarget) WPTarget, SUM(taiq.WPAchievement) WPAchievement, taiq.WPThreshold,
+taiq.RedLine, taiq.EverBilled, taiq.WithPack, taiq.NewProduct,
+ISNULL(taiq.AchievementUBL,0) AchievementUBL, ISNULL(taiq.AchievementUCL,0)AchievementUCL
 
-FROM IQReport_11292021002037 iqr
-JOIN Employees emp on iqr.SRID = emp.EmployeeID
-JOIN JCMonth AS j ON j.JCMonthID = iqr.JCMonthID
-LEFT JOIN IQReportCompanyWise iqrc ON iqrc.JCMonthID=iqr.JCMonthID AND iqrc.SRID =iqr.SRID 
-LEFT JOIN IQPerformanceKPISetup AS ik ON ik.StartDate = j.JCMonthStartDate AND ik.EndDate = j.JCMonthEndDate 
+FROM View_TargetAchievementIQ taiq
 
-WHERE iqr.JCYearID = @JCYearID AND iqr.JCMonthID = @JCMonthID AND emp.SalesPointID = @SalesPointID
+WHERE taiq.JCYear = @JCYearID AND taiq.JCMonth = @JCMonthID AND taiq.SalesPointID = @SalesPointID
 
 GROUP BY
-iqr.EBThreshold,iqr.RLThreshold, iqr.NPDThreshold, iqr.WPThreshold,
-ik.RedLine, ik.EverBilled, ik.WithPack, ik.NewProduct, iqrc.AchievementUBL,iqrc.AchievementUCL
+taiq.EBThreshold,taiq.RLThreshold, taiq.NPDThreshold, taiq.WPThreshold,
+taiq.RedLine, taiq.EverBilled, taiq.WithPack, taiq.NewProduct, taiq.AchievementUBL,taiq.AchievementUCL
 
