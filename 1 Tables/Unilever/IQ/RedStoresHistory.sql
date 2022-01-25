b61a7dd6247d6f2ac09a1b24b3d4c9f2814d1876
ashiqul <ashiqul33@gmail.com>
2022-01-25 20:50:13 +0600

    table RedStoresHistory update

--- 1 Tables/Unilever/IQ/RedStoresHistory.sql
+++ 1 Tables/Unilever/IQ/RedStoresHistory.sql
@@ -9,16 +9,11 @@ CREATE TABLE [dbo].[RedStoresHistory](
 	SPCode varchar(15) NOT NULL,
 	OutletID int NOT NULL,
 	OutletCode varchar(15) NOT NULL,
-	SRID int NOT NULL,
-	ProductID int NOT NULL,
-	[Target] money NOT NULL,
-	Achievement money NOT NULL,
-	Threshold money NOT NULL,
-	PercentAchievement money NOT NULL,
+	TargetLine int NOT NULL,
+	AchievementLine int NOT NULL,
 	CONSTRAINT [RedStoresHistoryID] PRIMARY KEY CLUSTERED
 	(
 		[RedStoresHistoryID] ASC
 	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
