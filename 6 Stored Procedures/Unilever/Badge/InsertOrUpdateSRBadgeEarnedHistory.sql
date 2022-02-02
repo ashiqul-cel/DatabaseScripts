2371a181080412a50076f2a939fbc98e04f5d542
ashiqul <ashiqul33@gmail.com>
2022-02-02 17:30:59 +0600

    bulk insert sr badge history procedure

---
+++ 6 Stored Procedures/Unilever/Badge/InsertOrUpdateSRBadgeEarnedHistory.sql
@@ -0,0 +1,19 @@
+USE [UnileverOS]
+GO
+
+CREATE PROCEDURE [dbo].[InsertOrUpdateSRBadgeEarnedHistory]
+@tblSRBadgeEarnedHistorys SRBadgeEarnedHistoryType READONLY
+AS
+SET NOCOUNT ON;
+
+BEGIN
+      MERGE INTO SRBadgeEarnedHistory seh
+      USING @tblSRBadgeEarnedHistorys tseh
+      ON seh.SRID = tseh.SRID AND seh.AchieveYear = tseh.AchieveYear
+      AND seh.AchieveMonth = tseh.AchieveMonth AND seh.BadgeEarnedID = tseh.BadgeEarnedID
+      WHEN MATCHED THEN
+      UPDATE SET seh.PointEarned = tseh.PointEarned
+      WHEN NOT MATCHED THEN
+      INSERT(SRID, AchieveYear, AchieveMonth, BadgeEarnedID, PointEarned)
+      VALUES(tseh.SRID, tseh.AchieveYear, tseh.AchieveMonth, tseh.BadgeEarnedID, tseh.PointEarned);
+END
