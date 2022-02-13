db2a82f40d67fbe5c6220a6586e5de745057b55b
ashiqul <ashiqul33@gmail.com>
2022-02-13 19:27:29 +0600

    update badge by srid

--- 6 Stored Procedures/Unilever/Badge/GetIQBadgesBySRID.sql
+++ 6 Stored Procedures/Unilever/Badge/GetIQBadgesBySRID.sql
@@ -1,14 +1,14 @@
 USE [UnileverOS]
 GO
 
-CREATE PROCEDURE [dbo].[GetIQBadgesBySRID]
+ALTER PROCEDURE [dbo].[GetIQBadgesBySRID]
 @SRID INT
 AS
 SET NOCOUNT ON;
 
 --DECLARE @SRID INT = 62431
 
-DECLARE @Year INT = YEAR(GETDATE())
+DECLARE @Year INT = YEAR(GETDATE()) - 1
 
 SELECT bd.BadgeDefinitionID BadgeSequence, bd.BadgeDescBangla, bd.BadgeImageLink,
 COUNT(bd.BadgeDefinitionID) BadgesAchievedCount, seh.PointEarned, seh.AchieveYear,
@@ -16,15 +16,20 @@ COUNT(bd.BadgeDefinitionID) BadgesAchievedCount, seh.PointEarned, seh.AchieveYea
 	CASE
 	WHEN bd.BadgeDefinitionID IN
 	(
-		SELECT TOP 2 seh.BadgeEarnedID FROM SRBadgeEarnedHistory AS seh
-		WHERE seh.SRID = @SRID AND seh.AchieveYear = @Year
-		ORDER BY seh.CreatedDate DESC, seh.PointEarned DESC
+		SELECT TOP 2 T1.BadgeEarnedID FROM 
+		(
+			SELECT seh.AchieveMonth, seh.BadgeEarnedID, seh.PointEarned,
+			COUNT(seh.BadgeEarnedID) OVER(PARTITION BY seh.BadgeEarnedID ORDER BY seh.AchieveMonth DESC) AS CountOfOrders
+			FROM SRBadgeEarnedHistory AS seh
+			WHERE seh.SRID = @SRID AND seh.AchieveYear = @Year
+		) T1 WHERE T1.CountOfOrders = 1
+		ORDER BY T1.AchieveMonth DESC, T1.PointEarned DESC
 	)
 	THEN 1 ELSE 0 END
-) IsRecentlyEarned
+) IsRecentlyEarned, bd.ColorCode
 FROM SRBadgeEarnedHistory AS seh
 INNER JOIN BadgeDefinition AS bd ON seh.BadgeEarnedID = bd.BadgeDefinitionID
 
 WHERE seh.SRID = @SRID AND seh.AchieveYear = @Year
-GROUP BY bd.BadgeDefinitionID, bd.BadgeDescBangla, bd.BadgeImageLink, seh.PointEarned, seh.AchieveYear
+GROUP BY bd.BadgeDefinitionID, bd.BadgeDescBangla, bd.BadgeImageLink, seh.PointEarned, seh.AchieveYear, bd.ColorCode
 ORDER BY bd.BadgeDefinitionID
