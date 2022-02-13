db7bc91365967cd48bbfe4c25e2543f31fc2bd77
ashiqul <ashiqul33@gmail.com>
2022-02-13 19:27:55 +0600

    update

--- 6 Stored Procedures/Unilever/Badge/GetTagrgetAchievementForBadgeIQ.sql
+++ 6 Stored Procedures/Unilever/Badge/GetTagrgetAchievementForBadgeIQ.sql
@@ -1,7 +1,7 @@
 USE [UnileverOS]
 GO
 
-CREATE PROCEDURE [dbo].[GetTagrgetAchievementForBadgeIQ]
+ALTER PROCEDURE [dbo].[GetTagrgetAchievementForBadgeIQ]
 @Year INT
 AS
 SET NOCOUNT ON;
@@ -10,10 +10,10 @@ SET NOCOUNT ON;
 
 SELECT
 iqr.[Year], iqr.[Month], emp.SalesPointId, iqr.SRID, emp.Code, emp.Code1,
-SUM(iqr.RLTarget * iqr.RLThreshold * ik.RedLine * 0.01) RLTarget, SUM(iqr.RLAchievement) RLAchievement,
-SUM(iqr.EBTarget * iqr.EBThreshold * ik.EverBilled * 0.01) EBTarget, SUM(iqr.EBAchievement) EBAchievement,
-SUM(iqr.WPTarget * iqr.WPThreshold * ik.WithPack * 0.01) WPTarget, SUM(iqr.WPAchievement) WPAchievement,
-SUM(iqr.NPDTarget * iqr.NPDThreshold * ik.NewProduct * 0.01) NPDTarget, SUM(iqr.NPDAchievement) NPDAchievement
+SUM(ISNULL(iqr.RLTarget * iqr.RLThreshold * ik.RedLine * 0.01, 0)) RLTarget, SUM(ISNULL(iqr.RLAchievement, 0)) RLAchievement,
+SUM(ISNULL(iqr.EBTarget * iqr.EBThreshold * ik.EverBilled * 0.01, 0)) EBTarget, SUM(ISNULL(iqr.EBAchievement, 0)) EBAchievement,
+SUM(ISNULL(iqr.WPTarget * iqr.WPThreshold * ik.WithPack * 0.01, 0)) WPTarget, SUM(ISNULL(iqr.WPAchievement, 0)) WPAchievement,
+SUM(ISNULL(iqr.NPDTarget * iqr.NPDThreshold * ik.NewProduct * 0.01, 0)) NPDTarget, SUM(ISNULL(iqr.NPDAchievement, 0)) NPDAchievement
 
 FROM IQReport iqr
 INNER JOIN Employees emp on iqr.SRID = emp.EmployeeID
