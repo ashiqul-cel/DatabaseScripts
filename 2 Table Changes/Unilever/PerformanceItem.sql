daca1893ab2e5d803659af70c0ccffba63b17c8f
Md Ashiqul Islam <ashiqul.cel@gmail.com>
2022-02-28 17:51:34 +0600

    alter table

--- 2 Table Changes/Unilever/PerformanceItem.sql
+++ 2 Table Changes/Unilever/PerformanceItem.sql
@@ -2,4 +2,10 @@ ALTER TABLE PerformanceItem
 ADD PreviousMonthAchieve INT NULL,
 	PreviousMonthPenalty INT NULL,
 	ConsecutiveMonthAchieve INT NULL,
-	ConsecutiveMonthPenalty INT NULL
+	ConsecutiveMonthPenalty INT NULL,
+	DependencyKPIId INT NULL,
+	DependencyKPIPercentage MONEY NULL
+
+ALTER TABLE PerformanceItem
+ADD DependencyKPIId INT NULL,
+	DependencyKPIPercentage MONEY NULL
