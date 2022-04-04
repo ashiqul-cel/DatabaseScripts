11028d9041115368909602b637dc140fec98dc9c
ashiqul <ashiqul33@gmail.com>
2022-04-04 15:43:23 +0600

    update

--- 6 Stored Procedures/Arla/getAllSalesPointsAllowedSalesWithMobile.sql
+++ 6 Stored Procedures/Arla/getAllSalesPointsAllowedSalesWithMobile.sql
@@ -3,7 +3,7 @@ AS
 SET NOCOUNT ON;
 
 SELECT sp.SalesPointID, sp.Code, sp.Name, sp.OfficeAddress, sp.[Status],
-IIF(ISNULL(msp.SalesPointID, 0) > 0, 1, 1) IsSelected
+IIF(ISNULL(msp.SalesPointID, 0) > 0 AND msp.[Status] = 16, 1, 0) IsSelected
 FROM SalesPoints AS sp
 LEFT JOIN MobileSalesEligibleSalespoint AS msp ON sp.SalesPointID = msp.SalesPointID
 WHERE sp.[Status] = 16
