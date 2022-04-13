dc48b92d8749d32b9c284eb8018c604b31433750
Md Ashiqul Islam <ashiqul.cel@gmail.com>
2022-04-13 16:27:28 +0600

    update

--- 6 Stored Procedures/Unilever/IQ/Save_RedStoresHistory.sql
+++ 6 Stored Procedures/Unilever/IQ/Save_RedStoresHistory.sql
@@ -22,14 +22,18 @@ DECLARE @tempRedStoresHistory TABLE (
 BEGIN
 	insert into @tempRedStoresHistory ([Year], [Month], SalesPointID, SPCode, OutletID, OutletCode, SRID, TargetLine, AchievementLine)
 
-	select iqta.Year, iqta.Month, sp.SalesPointID, rs.DistributorCode, c.CustomerID, rs.OutletCode, iqta.SRID,
-	count(iqta.Target) [TargetLine], sum(case when iqta.Achievement >= iqta.Target then 1 else 0 end) AchievementLine
-	from RedStores rs
-	inner join SalesPoints sp on rs.DistributorCode = sp.Code
-	inner join Customers c on rs.OutletCode = c.Code and sp.SalesPointID = c.SalesPointID
-	inner join IQTargetAchievement iqta on c.CustomerID = iqta.OutletID and sp.SalesPointID = iqta.SalesPointID
-	where iqta.Year = @PresentYear and iqta.Month = @PresentMonth
-	group by iqta.Year, iqta.Month, sp.SalesPointID, rs.DistributorCode, c.CustomerID, rs.OutletCode, iqta.SRID
+	SELECT ir.Year, ir.Month, sp.SalesPointID, rs.DistributorCode, c.CustomerID, rs.OutletCode, ir.SRID,
+	CEILING(ir.EBTarget * ir.EBThreshold * 0.01 + ir.RLTarget * ir.RLThreshold * 0.01 + ir.NPDTarget * ir.NPDThreshold * 0.01 + ir.WPTarget * ir.WPThreshold * 0.01) TargetLine, 
+	(ir.EBAchievement + ir.RLAchievement + ir.NPDAchievement + ir.WPAchievement) AchievementLine
+	from 
+	(
+		SELECT DISTINCT rs.OutletCode, rs.DistributorCode FROM RedStores rs
+		WHERE CAST(@Present AS DATE) BETWEEN CAST(rs.StartDate AS DATE) AND CAST(rs.EndDate AS DATE)
+	) rs
+	INNER JOIN SalesPoints sp on rs.DistributorCode = sp.Code
+	INNER JOIN Customers c on rs.OutletCode = c.Code and sp.SalesPointID = c.SalesPointID
+	INNER JOIN IQReport ir ON c.CustomerID = ir.OutletID
+	WHERE ir.Year = @PresentYear AND ir.Month = @PresentMonth
 
 	MERGE INTO RedStoresHistory AS rsh
 	USING @tempRedStoresHistory AS src
