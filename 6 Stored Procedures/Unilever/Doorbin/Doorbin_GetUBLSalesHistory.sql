46c00b43e4e79ee826b32364a98c4361a562ed9d
ashiqul <ashiqul33@gmail.com>
2022-04-18 15:08:12 +0600

    update version 2

--- 6 Stored Procedures/Unilever/Doorbin/Doorbin_GetUBLSalesHistory.sql
+++ 6 Stored Procedures/Unilever/Doorbin/Doorbin_GetUBLSalesHistory.sql
@@ -9,7 +9,8 @@ SET NOCOUNT ON;
 SELECT SI.CustomerID OutletID, SII.SKUID, 
 FLOOR(SII.Quantity / SK.CartonPcsRatio) Carton, (SII.Quantity % SK.CartonPcsRatio) Piece,
 (SII.Quantity * SII.TradePrice) Total, 0 TkOff, CONVERT(VARCHAR, SI.InvoiceDate, 111) OrderDate,
-ROW_NUMBER() OVER(ORDER BY (SELECT 1)) AS NoOfOrder, 0 TPRID, 0 ItemID, SK.Name AS SKUName, 0 ISB2B
+DENSE_RANK() OVER(PARTITION BY SI.CustomerID ORDER BY CAST(SI.InvoiceDate AS DATE) DESC) AS NoOfOrder,
+0 TPRID, 0 ItemID, SK.Name AS SKUName, 0 ISB2B
 
 FROM SalesInvoices AS SI 
 INNER JOIN SalesInvoiceItem AS SII ON SII.InvoiceID = SI.InvoiceID
@@ -18,7 +19,8 @@ INNER JOIN SKUs AS SK ON SK.SKUID = SII.SKUID
 WHERE SI.SalesPointID = @SalesPointID AND SI.CustomerID = @OutletID 
 AND SI.InvoiceID IN
 (
-	SELECT TOP 3 SI2.InvoiceID FROM SalesInvoices SI2 
+	SELECT TOP 3 SI2.InvoiceID FROM SalesInvoices SI2
 	WHERE SI2.CustomerID = @OutletID AND SI2.GrossValue > 0
+	order by SI2.InvoiceDate desc
 )
 AND SII.Quantity > 0
