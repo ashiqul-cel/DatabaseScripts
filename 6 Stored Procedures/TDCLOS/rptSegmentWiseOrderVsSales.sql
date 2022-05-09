6f69213d7af5b84e6d0e903980d8cf26c0bd0b2b
Md Ashiqul Islam <ashiqul.cel@gmail.com>
2022-05-09 16:08:16 +0600

    update

--- 6 Stored Procedures/TDCLOS/rptSegmentWiseOrderVsSales.sql
+++ 6 Stored Procedures/TDCLOS/rptSegmentWiseOrderVsSales.sql
@@ -1,50 +1,121 @@
-CREATE PROCEDURE [dbo].[rptSegmentWiseOrderVsSales]
-@SalesPointID INT, @StartDate DATETIME, @EndDate DATETIME
-AS
-SET NOCOUNT ON; 
+--ALTER PROCEDURE [dbo].[rptSegmentWiseOrderVsSales]
+--@SalesPointID INT, @StartDate DATETIME, @EndDate DATETIME
+--AS
+--SET NOCOUNT ON;
 
---DECLARE @SalesPointID INT = 1, @StartDate DATETIME = '1 Oct 2021', @EndDate DATETIME = '31 Oct 2021';
+DECLARE @SalesPointID INT = 1, @StartDate DATETIME = '1 Oct 2021', @EndDate DATETIME = '31 Oct 2021';
 
-SELECT MHN.Name [National], MHR.Name Region, MHT.Name Territory, E.Code SRCode, E.Name SRName,
-T1.OrderDate, T1.GMemoCountOrder, T1.GOrderTotVal, T1.LMemoCountOrder,T1.LOrderTotVal, 
-(T1.GOrderTotVal + T1.LOrderTotVal)TotOrderVal, (T1.GMemoCountOrder + T1.LMemoCountOrder)OrderMemoCount,
+DECLARE @tmpOrders TABLE
+(
+	SRID INT NOT NULL,
+	SalesPointID INT NOT NULL,
+	OrderID INT NOT NULL,
+	OrderDate DATETIME  NOT NULL,
+	Quantity INT NOT NULL,
+	InvoicePrice MONEY NOT NULL,
+	NodeID INT NOT NULL
+)
+INSERT INTO @tmpOrders
+(SRID, SalesPointID, OrderID, OrderDate, Quantity, InvoicePrice, NodeID)
+SELECT so.SRID, so.SalesPointID, so.OrderID, so.OrderDate, soi.Quantity, soi.InvoicePrice, ph2.NodeID
+FROM SalesOrders AS so
+INNER JOIN SalesOrderItem AS soi ON so.OrderID = soi.OrderID
+INNER JOIN SKUs AS s ON s.SKUID = soi.SKUID
+INNER JOIN ProductHierarchies AS ph1 ON s.ProductID = ph1.NodeID AND ph1.LevelID = 3
+INNER JOIN ProductHierarchies AS ph2 ON ph2.NodeID = ph1.ParentID AND ph2.LevelID = 2
+WHERE so.OrderDate BETWEEN @StartDate AND @EndDate AND so.SalesPointID = @SalesPointID AND ph2.NodeID IN (2,3)
 
-T2.GMemoCountSales, T2.GSalesTotVal, T2.LMemoCountSales, T2.LSalesTotVal,
-(T2.GSalesTotVal + T2.LSalesTotVal) TotSalesVal, (T2.GMemoCountSales + T2.LMemoCountSales) SalesMemoCount
+
+DECLARE @tmpSales TABLE
+(
+	SRID INT NOT NULL,
+	SalesPointID INT NOT NULL,
+	OrderID INT NOT NULL,
+	InvoiceID INT NOT NULL,
+	Quantity INT NOT NULL,
+	InvoicePrice MONEY NOT NULL,
+	NodeID INT NOT NULL
+)
+INSERT INTO @tmpSales
+(SRID, SalesPointID, OrderID, InvoiceID, Quantity, InvoicePrice, NodeID)
+SELECT si.SRID, si.SalesPointID, si.OrderID, si.InvoiceID, sii.Quantity, sii.InvoicePrice, ph2.NodeID
+FROM SalesInvoices AS si
+INNER JOIN SalesInvoiceItem AS sii ON si.InvoiceID = sii.InvoiceID
+INNER JOIN SKUs AS s ON s.SKUID = sii.SKUID
+INNER JOIN ProductHierarchies AS ph1 ON s.ProductID = ph1.NodeID AND ph1.LevelID = 3
+INNER JOIN ProductHierarchies AS ph2 ON ph2.NodeID = ph1.ParentID AND ph2.LevelID = 2
+WHERE si.OrderDate BETWEEN @StartDate AND @EndDate AND si.SalesPointID = @SalesPointID AND ph2.NodeID IN (2,3) AND si.OrderID IS NOT NULL
+
+
+SELECT MH.[National], MH.Region, MH.Territory, MH.[Distributor Code], MH.Distributor, E.Code SRCode, E.Name SRName, T12.OrderDate,
+
+ISNULL(T12.GMemoCountOrder, 0) GMemoCountOrder, ISNULL(T12.GOrderTotVal, 0) GOrderTotVal,
+ISNULL(T13.LMemoCountOrder, 0) LMemoCountOrder, ISNULL(T13.LOrderTotVal, 0) LOrderTotVal,
+ISNULL(T11.TotOrderVal, 0) TotOrderVal, ISNULL(T11.OrderMemoCount, 0) OrderMemoCount,
+
+ISNULL(T21.GMemoCountSales, 0) GMemoCountSales, ISNULL(T21.GSalesTotVal, 0) GSalesTotVal,
+ISNULL(T22.LMemoCountSales, 0) LMemoCountSales, ISNULL(T22.LSalesTotVal, 0) LSalesTotVal,
+ISNULL(T23.TotSalesVal, 0) TotSalesVal, ISNULL(T23.SalesMemoCount, 0) SalesMemoCount
 FROM
 (
 	SELECT so.SRID, so.SalesPointID, so.OrderDate, so.OrderID,
-	IIF(ph2.NodeID = 2, COUNT(DISTINCT so.OrderID), 0)GMemoCountOrder,
-	ISNULL(SUM(IIF(ph2.NodeID = 2, soi.Quantity * soi.InvoicePrice, 0)), 0)GOrderTotVal,
-	IIF(ph2.NodeID = 3, COUNT(DISTINCT so.OrderID), 0)LMemoCountOrder,
-	ISNULL(SUM(IIF(ph2.NodeID = 3, soi.Quantity * soi.InvoicePrice, 0)), 0)LOrderTotVal
-	FROM SalesOrders AS so
-	INNER JOIN SalesOrderItem AS soi ON so.OrderID = soi.OrderID
-	INNER JOIN SKUs AS s ON s.SKUID = soi.SKUID
-	INNER JOIN ProductHierarchies AS ph1 ON s.BrandID = ph1.NodeID AND ph1.LevelID = 3
-	INNER JOIN ProductHierarchies AS ph2 ON ph2.NodeID = ph1.ParentID AND ph2.LevelID = 2
-	WHERE so.OrderDate BETWEEN @StartDate AND @EndDate AND ph2.NodeID IN (2,3)
-	AND so.SalesPointID = @SalesPointID
-	GROUP BY so.OrderDate, so.OrderID, so.SRID, so.SalesPointID, ph2.NodeID
-)T1
+	ISNULL(COUNT(DISTINCT so.OrderID), 0) OrderMemoCount,
+	ISNULL(SUM(so.Quantity * so.InvoicePrice), 0) TotOrderVal
+	FROM @tmpOrders so
+	GROUP BY so.OrderDate, so.OrderID, so.SRID, so.SalesPointID
+)T11
+LEFT JOIN
+(
+	SELECT so.SRID, so.SalesPointID, so.OrderDate, so.OrderID,
+	ISNULL(COUNT(DISTINCT so.OrderID), 0) GMemoCountOrder,
+	ISNULL(SUM(so.Quantity * so.InvoicePrice), 0) GOrderTotVal
+	FROM @tmpOrders so
+	WHERE so.NodeID = 2
+	GROUP BY so.OrderDate, so.OrderID, so.SRID, so.SalesPointID
+)T12 ON T11.OrderID = T12.OrderID
+LEFT JOIN
+(
+	SELECT so.SRID, so.SalesPointID, so.OrderDate, so.OrderID,
+	ISNULL(COUNT(DISTINCT so.OrderID), 0) LMemoCountOrder,
+	ISNULL(SUM(so.Quantity * so.InvoicePrice), 0) LOrderTotVal
+	FROM @tmpOrders so
+	WHERE so.NodeID = 3
+	GROUP BY so.OrderDate, so.OrderID, so.SRID, so.SalesPointID
+)T13 ON T11.OrderID = T13.OrderID
 LEFT JOIN
 (
 	SELECT si.SRID, si.SalesPointID, si.OrderID,
-	IIF(ph2.NodeID = 2, COUNT(DISTINCT si.InvoiceID), 0)GMemoCountSales,
-	ISNULL(SUM(IIF(ph2.NodeID = 2, sii.Quantity * sii.InvoicePrice, 0)), 0)GSalesTotVal,
-	IIF(ph2.NodeID = 3, COUNT(DISTINCT si.OrderID), 0)LMemoCountSales,
-	ISNULL(SUM(IIF(ph2.NodeID = 3, sii.Quantity * sii.InvoicePrice, 0)), 0)LSalesTotVal
-	FROM SalesInvoices AS si
-	INNER JOIN SalesInvoiceItem AS sii ON si.InvoiceID = sii.InvoiceID
-	INNER JOIN SKUs AS s ON s.SKUID = sii.SKUID
-	INNER JOIN ProductHierarchies AS ph1 ON s.BrandID = ph1.NodeID AND ph1.LevelID = 3
-	INNER JOIN ProductHierarchies AS ph2 ON ph2.NodeID = ph1.ParentID AND ph2.LevelID = 2
-	WHERE ph2.NodeID IN (2,3) AND si.SalesPointID = @SalesPointID
-	GROUP BY si.OrderID, si.SRID, si.SalesPointID, ph2.NodeID
-)T2 ON T1.OrderID = T2.OrderID
-INNER JOIN SalesPoints SP ON SP.SalesPointID = T1.SalesPointID
-INNER JOIN SalesPointMHNodes SPMH ON SPMH.SalesPointID = SP.SalesPointID
-INNER JOIN MHNode MHT ON SPMH.NodeID = MHT.NodeID
-INNER JOIN MHNode MHR ON MHT.ParentID = MHR.NodeID
-INNER JOIN MHNode MHN ON MHR.ParentID = MHN.NodeID
-INNER JOIN Employees AS e ON e.EmployeeID = T1.SRID
+	ISNULL(COUNT(DISTINCT si.InvoiceID), 0) GMemoCountSales,
+	ISNULL(SUM(si.Quantity * si.InvoicePrice), 0) GSalesTotVal
+	FROM @tmpSales AS si
+	WHERE si.NodeID = 2
+	GROUP BY si.OrderID, si.SRID, si.SalesPointID
+)T21 ON T11.OrderID = T21.OrderID
+LEFT JOIN
+(
+	SELECT si.SRID, si.SalesPointID, si.OrderID,
+	ISNULL(COUNT(DISTINCT si.InvoiceID), 0) LMemoCountSales,
+	ISNULL(SUM(si.Quantity * si.InvoicePrice), 0) LSalesTotVal
+	FROM @tmpSales AS si
+	WHERE si.NodeID = 3
+	GROUP BY si.OrderID, si.SRID, si.SalesPointID
+)T22 ON T11.OrderID = T22.OrderID
+LEFT JOIN
+(
+	SELECT si.SRID, si.SalesPointID, si.OrderID,
+	ISNULL(COUNT(DISTINCT si.InvoiceID), 0) SalesMemoCount,
+	ISNULL(SUM(si.Quantity * si.InvoicePrice), 0) TotSalesVal
+	FROM @tmpSales AS si
+	GROUP BY si.OrderID, si.SRID, si.SalesPointID
+)T23 ON T11.OrderID = T23.OrderID
+INNER JOIN Employees AS e ON e.EmployeeID = T12.SRID
+INNER JOIN
+(
+	SELECT sp.SalesPointID, MHN.Name [National], MHR.Name Region, MHT.Name Territory, sp.Code [Distributor Code], sp.Name Distributor
+	FROM SalesPoints AS sp
+	INNER JOIN SalesPointMHNodes SPMH ON SPMH.SalesPointID = sp.SalesPointID
+	INNER JOIN MHNode MHT ON SPMH.NodeID = MHT.NodeID
+	INNER JOIN MHNode MHR ON MHT.ParentID = MHR.NodeID
+	INNER JOIN MHNode MHN ON MHR.ParentID = MHN.NodeID
+	WHERE sp.SalesPointID = @SalesPointID
+) MH ON T11.SalesPointID = MH.SalesPointID
