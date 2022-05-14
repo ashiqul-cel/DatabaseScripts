a77bbf89652a7f6d5ee669e0dc92c54ad9dc784b
Md Ashiqul Islam <ashiqul.cel@gmail.com>
2022-05-14 14:28:01 +0600

    update report procedure

---
+++ 6 Stored Procedures/TDCL_OtherBrand/Get_TpClaimSummaryDumpDataV2.sql
@@ -0,0 +1,21 @@
+ALTER PROCEDURE [dbo].[Get_TpClaimSummaryDumpDataV2]  @StartDate DATETIME, @EndDate DATETIME 
+AS
+
+--DECLARE @StartDate DATETIME = '1 Mar 2022', @EndDate DATETIME = '31 Mar 2022'
+
+SELECT MR.Code RegionCode,MR.Name Region,MT.Code TerritoryCode ,MT.Name Territory, A.SalesPointCode DBCode,A.SalesPointName DBName,A.SRCode,A.SRName,
+A.SRContactNo,A.RouteCode,A.RouteName,A.OutletCode,A.OutletName,A.ChannelCode Channel,A.ChannelName CustomerType,
+A.InvoiceNo,A.TranDate InvoiceDate,A.Promotion, A.StartDate [From] ,A.EndDate [To], ISNULL(A.CompanyName, '') CompanyName, A.SlabNo TPSlabNo,
+SUM(A.TotalSales) TotalSalesValue,SUM(A.TotalSalespcs) TotalSalesPcs,SUM(A.TotalSales - A.PromoSalesValue) TotalNonPromoSales, 
+SUM(A.Claimpcs) ClaimPcs,SUM(A.ClaimValue) ClaimValue,SUM(A.TotalSales - A.ClaimValue) TotalActualSales
+
+FROM Daily_TP_Claim_Summary_Data AS A
+INNER JOIN SalesPointMHNodes as spm on spm.SalesPointID = A.SalesPointID
+INNER JOIN MHNode MT ON MT.NodeID = SPM.NodeID
+INNER JOIN MHNode MR ON MR.NodeID = MT.ParentID
+
+WHERE A.TranDate BETWEEN @StartDate and @EndDate
+
+group by MR.Code,MR.Name,MT.Code,MT.Name, A.SalesPointCode,A.SalesPointName,A.SRCode,A.SRName,A.SRContactNo,A.RouteCode,A.RouteName,
+A.OutletCode,A.OutletName,A.InvoiceNo,A.TranDate,A.Promotion, A.StartDate ,A.EndDate,A.SlabNo,A.ChannelCode,A.ChannelName, A.CompanyName
+order by StartDate, SalesPointCode, Promotion,SlabNo
