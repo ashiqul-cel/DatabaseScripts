d590248ea2019d27ecc22e01e5e0b1cff47c7046
Md Ashiqul Islam <ashiqul.cel@gmail.com>
2022-06-01 15:24:13 +0600

    added order date and order no

--- 6 Stored Procedures/TDCL_OtherBrand/rptInvoiceWisePromotionDetails.sql
+++ 6 Stored Procedures/TDCL_OtherBrand/rptInvoiceWisePromotionDetails.sql
@@ -1,9 +1,9 @@
-CREATE PROCEDURE [dbo].[rptInvoiceWisePromotionDetails]
+ALTER PROCEDURE [dbo].[rptInvoiceWisePromotionDetails]
 @SalespointIDs VARCHAR(MAX), @FromDate DATETIME, @ToDate DATETIME
 AS
 SET NOCOUNT ON;
 
--- DECLARE @SalespointIDs VARCHAR(MAX) = '33', @FromDate DATETIME = '1 Mar 2022', @ToDate DATETIME = '31 Mar 2022'
+--DECLARE @SalespointIDs VARCHAR(MAX) = '33', @FromDate DATETIME = '1 Mar 2022', @ToDate DATETIME = '31 Mar 2022'
 
 DECLARE @tmpSalesInvoices TABLE
 (
@@ -13,12 +13,15 @@ DECLARE @tmpSalesInvoices TABLE
 	SalesPointID INT NOT NULL,
 	CustomerID INT NOT NULL,
 	GrossValue MONEY NOT NULL,
-	NetValue MONEY NOT NULL
+	NetValue MONEY NOT NULL,
+	OrderDate DATETIME NULL,
+	OrderNo VARCHAR(25) NULL
 )
 INSERT INTO @tmpSalesInvoices
-(InvoiceID, InvoiceNo, InvoiceDate, SalesPointID, CustomerID, GrossValue, NetValue)
-SELECT si.InvoiceID, si.InvoiceNo, si.InvoiceDate, si.SalesPointID, si.CustomerID, si.GrossValue, si.NetValue
+(InvoiceID, InvoiceNo, InvoiceDate, SalesPointID, CustomerID, GrossValue, NetValue, OrderDate, OrderNo)
+SELECT si.InvoiceID, si.InvoiceNo, si.InvoiceDate, si.SalesPointID, si.CustomerID, si.GrossValue, si.NetValue, so.OrderDate, so.OrderNo
 FROM SalesInvoices AS si
+LEFT JOIN SalesOrders AS so ON so.OrderID = si.OrderID
 WHERE CAST(si.InvoiceDate AS DATE) BETWEEN CAST(@FromDate AS DATE) AND CAST(@ToDate AS DATE)
 AND si.SalesPointID IN (SELECT * FROM STRING_SPLIT(@SalesPointIDs, ','))
 
@@ -26,14 +29,14 @@ SELECT T1.*,isnull(t2.Discount,0)Discount, (T1.GrossValue - isnull(t2.Discount,0
 ISNULL(t3.FreeQty,0)FreeQty, ISNULL(T4.GiftQty, 0)GiftQty FROM 
 (
 	SELECT sp.Name SalesPointName,si.InvoiceID,si.InvoiceNo,c.Code CustomerCode,c.Name CustomerName, 
-	c.Address1, c.OwnerName, c.ContactNo,SUM(sii.Quantity) Quantity, si.GrossValue, si.InvoiceDate, sp.OfficeAddress
+	c.Address1, c.OwnerName, c.ContactNo,SUM(sii.Quantity) Quantity, si.GrossValue, si.InvoiceDate, sp.OfficeAddress, si.OrderDate, si.OrderNo
 	FROM @tmpSalesInvoices AS si
 	INNER JOIN SalesInvoiceItem AS sii ON sii.InvoiceID = si.InvoiceID
 	INNER JOIN SKUs AS s ON s.SKUID = sii.SKUID
 	INNER JOIN Customers AS c ON c.CustomerID=si.CustomerID
 	INNER JOIN SalesPoints AS sp ON si.SalesPointID = sp.SalesPointID
 	GROUP BY  sp.Name ,c.Name,si.GrossValue, si.NetValue,c.Code,si.InvoiceNo,si.InvoiceID, si.InvoiceDate,
-	c.Address1, c.OwnerName, c.ContactNo, sp.OfficeAddress
+	c.Address1, c.OwnerName, c.ContactNo, sp.OfficeAddress, si.OrderDate, si.OrderNo
 )T1 LEFT JOIN 
 (
 	SELECT SUM(sip.BonusValue) Discount,si.InvoiceID
