2022dff06c75005ab09e84dfcfddaf10cc9cf9b7
Md Ashiqul Islam <ashiqul.cel@gmail.com>
2022-06-02 09:37:29 +0600

    update badge image url

--- 1 Tables/Unilever/Badge/insertData/BadgeDefinition.sql
+++ 1 Tables/Unilever/Badge/insertData/BadgeDefinition.sql
@@ -16,10 +16,10 @@
 	CreatedBy
 )
 VALUES
-( 1, 'Perfect Opener', N'পারফেক্ট ওপেনার', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.212.8.217/ublosdsservice/assets/iqbadge/perfect-opener.png', NULL, 0, 1, 3, '#eaf8ff', 16, -9 ),
-( 2, 'Perfect Century', N'পারফেক্ট সেঞ্চুরি', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.212.8.217/ublosdsservice/assets/iqbadge/century.png', NULL, 1, 12, 3, '#f6eff9', 16, -9 ),
-( 3, 'Cutter-Master', N'কাটার-মাস্টার	', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.212.8.217/ublosdsservice/assets/iqbadge/katar.png', 2, 2, 11, 5, '#eafbfa', 16, -9 ),
-( 4, 'King of Slog-Sweep', N'স্লগ-সুইপ এর রাজা', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.212.8.217/ublosdsservice/assets/iqbadge/slog.png', 2, 3, 10, 7, '#fff4f4', 16, -9 ),
-( 5, 'Perfect Googly', N'পারফেক্ট গুগলি', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.212.8.217/ublosdsservice/assets/iqbadge/googly.png', 2, 6, 7, 15, '#eaf1fd', 16, -9 ),
-( 6, 'Perfect Doosra', N'পারফেক্ট দুসরা', '3,4', 30, 'IQ E2E (Easy to Earn)', 'http://20.212.8.217/ublosdsservice/assets/iqbadge/dosra.png', NULL, 1, 12, 20, '#ffeaf4', 16, -9 ),
-( 7, 'The Best All-rounder', N'সেরা অল-রাউন্ডার', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.212.8.217/ublosdsservice/assets/iqbadge/shera.png', 2, 12, 1, 50, '#fff7ec', 16, -9 )
+( 1, 'Perfect Opener', N'পারফেক্ট ওপেনার', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.24.28.218/ublosdsservice/assets/iqbadge/perfect-opener.png', NULL, 0, 1, 3, '#eaf8ff', 16, -9 ),
+( 2, 'Perfect Century', N'পারফেক্ট সেঞ্চুরি', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.24.28.218/ublosdsservice/assets/iqbadge/century.png', NULL, 1, 12, 3, '#f6eff9', 16, -9 ),
+( 3, 'Cutter-Master', N'কাটার-মাস্টার	', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.24.28.218/ublosdsservice/assets/iqbadge/katar.png', 2, 2, 11, 5, '#eafbfa', 16, -9 ),
+( 4, 'King of Slog-Sweep', N'স্লগ-সুইপ এর রাজা', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.24.28.218/ublosdsservice/assets/iqbadge/slog.png', 2, 3, 10, 7, '#fff4f4', 16, -9 ),
+( 5, 'Perfect Googly', N'পারফেক্ট গুগলি', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.24.28.218/ublosdsservice/assets/iqbadge/googly.png', 2, 6, 7, 15, '#eaf1fd', 16, -9 ),
+( 6, 'Perfect Doosra', N'পারফেক্ট দুসরা', '3,4', 30, 'IQ E2E (Easy to Earn)', 'http://20.24.28.218/ublosdsservice/assets/iqbadge/dosra.png', NULL, 1, 12, 20, '#ffeaf4', 16, -9 ),
+( 7, 'The Best All-rounder', N'সেরা অল-রাউন্ডার', '1,2,3,4', 26, 'IQ Incentive Monthly (TA)', 'http://20.24.28.218/ublosdsservice/assets/iqbadge/shera.png', 2, 12, 1, 50, '#fff7ec', 16, -9 )
