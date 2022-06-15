627309bd2213c0b7e540241ea8b894a751e24f8d
Md Ashiqul Islam <ashiqul.cel@gmail.com>
2022-06-15 18:12:34 +0600

    update proc

--- 6 Stored Procedures/TDCL_OtherBrand/GetSKUWiseSuggestedTarget.sql
+++ 6 Stored Procedures/TDCL_OtherBrand/GetSKUWiseSuggestedTarget.sql
@@ -1,11 +1,12 @@
-CREATE PROCEDURE [dbo].[GetSKUWiseSuggestedTarget]
-@NoOfLastMonth INT
+ALTER PROCEDURE [dbo].[GetSKUWiseSuggestedTarget]
+@NoOfLastMonth INT, @Month INT, @Year INT
 AS
 SET NOCOUNT ON;
 
---DECLARE @NoOfLastMonth INT = 3
+--DECLARE @NoOfLastMonth INT = 3, @Month INT = 6, @Year INT = 2022
 
-DECLARE @StartDate DATE = GETDATE() - @NoOfLastMonth * 30, @EndDate DATE = GETDATE()
+DECLARE @SelectDate DATE = DATEFROMPARTS(@Year, @Month, 1)
+DECLARE @StartDate DATE = DATEADD(MONTH, -@NoOfLastMonth, @SelectDate), @EndDate DATE = DATEADD(DAY, -1, @SelectDate)
 
 SELECT sii.SKUID, CAST(ROUND(SUM(sii.Quantity) / @NoOfLastMonth, 0) AS INT) SuggestedTarget
 FROM SalesInvoices AS si
